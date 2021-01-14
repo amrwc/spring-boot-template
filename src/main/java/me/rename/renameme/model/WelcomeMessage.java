175e77b157f92b03bf847c778e91e72fe4232c03
amrwc <42574496+amrwc@users.noreply.github.com>
2021-01-14 01:30:59 +0000

    Improve `WelcomeController`

--- src/main/java/me/rename/renameme/model/WelcomeMessage.java
+++ src/main/java/me/rename/renameme/model/WelcomeMessage.java
@@ -13,13 +13,18 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "WELCOME_MESSAGES")
+@SequenceGenerator(name = "seq", sequenceName = "WELCOME_MESSAGES_SEQ", allocationSize = 1)
 public class WelcomeMessage {
 
     @Id
-    @GeneratedValue
     @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
     private Long id;
 
     @Column(name = "CONTENT", length = 64)
     private String content;
+
+    public WelcomeMessage(final String content) {
+        this.content = content;
+    }
 }
---
+++ src/main/java/me/rename/renameme/request/WelcomeMessageRequest.java
@@ -0,0 +1,11 @@
+package me.rename.renameme.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WelcomeMessageRequest {
+
+    private String content;
+}
